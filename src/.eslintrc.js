4c336caa714f615125e5d58f166e2f51451eb786
parasSetschel <paras.bhesaniya@satschel.com>
2023-07-11 15:21:57 +0530

    errorboundry-with-usersAndPost-API

---
+++ src/.eslintrc.js
@@ -0,0 +1,12 @@
+const { Module } = require('module');
+
+Module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+};
